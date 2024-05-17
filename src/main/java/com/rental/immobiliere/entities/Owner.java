ffe68072e5af2cdbc66c981f77b546ac8d71c1da
Abidi Yassine <98038872+AbidiYassine-etu@users.noreply.github.com>
2024-05-17 10:03:19 +0100

    Add files via upload

---
+++ src/main/java/com/rental/immobiliere/entities/Owner.java
@@ -0,0 +1,28 @@
+package com.rental.immobiliere.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name ="owner")
+public class Owner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idOwner;
+    private String name;
+    private String contactInformation;
+
+
+
+    @OneToOne(mappedBy = "owner")
+    private Rental rental;
+
+
+
+
+}
